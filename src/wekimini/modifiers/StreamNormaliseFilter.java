fb4f08b6378c769b071f74d6204815ba4539a8b1
LouisMcCallum <louis.mccallum@heresyai.com>
2018-03-19 12:11:37 +0000

    Stream filter + tests

---
+++ src/wekimini/modifiers/StreamNormaliseFilter.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wekimini.modifiers;
+import java.util.HashMap;
+import weka.core.*;
+import weka.core.Capabilities.*;
+import weka.filters.*;
+/**
+ *
+ * @author louismccallum
+ */
+public class StreamNormaliseFilter extends SimpleStreamFilter {
+    
+    public BatchNormaliseFilter batchFilter;
+    
+    @Override
+    public String globalInfo() {
+        return "A filter to normalise an Instance, attribute by attribute, given a"
+                + "completed batch filter";
+    }
+
+    @Override
+    protected Instances determineOutputFormat(Instances i){
+        return i;
+    }
+
+    @Override
+    protected Instance process(Instance instance) {
+        double[] newVals = new double[instance.numAttributes()];
+        for(int j = 0; j < instance.numAttributes(); j++)
+        {
+            Attribute a = instance.attribute(j);
+            double val = instance.value(j);
+            double minVal = batchFilter.minForAttribute(a);
+            double maxVal = batchFilter.maxForAttribute(a);
+            double delta =  maxVal - minVal;
+            if(j != instance.classIndex())
+            {
+                newVals[j] = (val - minVal) / delta;
+            }
+            else
+            {
+                newVals[j] = val;
+            }
+        }
+        return new Instance(1, newVals);
+    }
+}
--- test/wekimini/NormaliseFilterTest.java
+++ test/wekimini/NormaliseFilterTest.java
@@ -16,6 +16,7 @@ import weka.core.Instance;
 import weka.core.Instances;
 import weka.filters.Filter;
 import wekimini.modifiers.BatchNormaliseFilter;
+import wekimini.modifiers.StreamNormaliseFilter;
 
 /**
  *
@@ -28,31 +29,37 @@ public class NormaliseFilterTest {
     @Before
     public void setup()
     {
+        testSet = getTestSet(100);
+    }
+    
+    public Instances getTestSet(int size)
+    {
         FastVector ff = new FastVector(3);
         ff.addElement(new Attribute("input1"));
         ff.addElement(new Attribute("input2"));
         ff.addElement(new Attribute("input3"));
         ff.addElement(new Attribute("output"));
-        testSet  = new Instances("dataset", ff, 100);
-        testSet.setClassIndex(3);
-        for(int i = 0; i <= 100; i++)
+        Instances set  = new Instances("dataset", ff, size);
+        set.setClassIndex(3);
+        for(int i = 0; i <= size; i++)
         {
             double[] vals = new double[4];
             vals[0] = i;
-            vals[1] = i - 50;
-            vals[2] = i - 100;
+            vals[1] = i - size / 2.0;
+            vals[2] = i - size;
             vals[3] = i;
-            testSet.add(new Instance(1, vals));
+            set.add(new Instance(1, vals));
         }
+        return set;
     }
     
     @Test
     public void testBatchFilter()
     {
-        BatchNormaliseFilter filter = new BatchNormaliseFilter();
+        BatchNormaliseFilter batchFilter = new BatchNormaliseFilter();
         try {
-            filter.setInputFormat(testSet);
-            testSet = Filter.useFilter(testSet, filter);
+            batchFilter.setInputFormat(testSet);
+            testSet = Filter.useFilter(testSet, batchFilter);
         } catch (Exception ex) {
             Logger.getLogger(NormaliseFilterTest.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -64,7 +71,30 @@ public class NormaliseFilterTest {
             assertEquals(i / 100, inst.value(2), 0.0);
             assertEquals(i, inst.value(3), 0.0);
         }
-        
     }
     
+    @Test
+    public void testStreamFilter()
+    {
+        BatchNormaliseFilter batchFilter = new BatchNormaliseFilter();
+        StreamNormaliseFilter streamFilter = new StreamNormaliseFilter();
+        try {
+            batchFilter.setInputFormat(testSet);
+            streamFilter.setInputFormat(testSet);
+            testSet = Filter.useFilter(testSet, batchFilter);
+            streamFilter.batchFilter = batchFilter;
+            Instances localSet = getTestSet(100);
+            localSet = Filter.useFilter(localSet, streamFilter);
+            for(double i = 0; i < localSet.numInstances(); i++)
+            {
+                Instance inst = localSet.instance((int)i);
+                assertEquals(i / 100, inst.value(0), 0.0);
+                assertEquals(i / 100, inst.value(1), 0.0);
+                assertEquals(i / 100, inst.value(2), 0.0);
+                assertEquals(i, inst.value(3), 0.0);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(NormaliseFilterTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
